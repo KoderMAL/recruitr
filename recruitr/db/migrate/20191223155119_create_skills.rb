aac3232951dadda24d988d5b25b5cc4f19cb2726
Amine <amine_lalaoui@hotmail.com>
2019-12-26 18:31:26 +0100

    readme

--- recruitr/db/migrate/20191223155119_create_skills.rb
+++ recruitr/db/migrate/20191223155119_create_skills.rb
@@ -2,7 +2,7 @@ class CreateSkills < ActiveRecord::Migration[6.0]
   def change
     create_table :skills do |t|
       t.string :content
-      t.reference :available_position, foreign_key: true
+      t.references :available_position, foreign_key: true
 
       t.timestamps
     end
